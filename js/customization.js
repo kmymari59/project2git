61368b923930d464d223484cf3114c7c502fcca2
kmymari59 <kmymari59@gmail.com>
2023-06-01 17:48:36 +0800

    更新-客製

--- js/customization.js
+++ js/customization.js
@@ -6,11 +6,36 @@ $(function () {
 
 
     /*
+    一版(現在會跟著一起動)
     onselect參考
     https://www.cnblogs.com/wxlkeepmoving/p/3162396.html
     
     https://blog.51cto.com/u_15064631/4027190
     */
+    const minus = $('.qtyminus');
+    const plus = $('.qtyplus');
+    const input = $('.qty');
+    minus.click(function (e) {
+        e.preventDefault();
+        var value = input.val();
+        if (value > 1) {
+            value--;
+        }
+        input.val(value);
+    });
+
+    plus.click(function (e) {
+        e.preventDefault();
+        var value = input.val();
+        value++;
+        input.val(value);
+    })
+
+    /*
+    參考 (可以分開的)
+    https://codepen.io/ahmetaksungur/pen/QWNEKXq
+    */
+
 
     
 });

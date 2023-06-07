3a4f3726ee0e2dfa0f5db9595d8271ab973ee6b0
kmymari59 <kmymari59@gmail.com>
2023-06-07 17:00:52 +0800

    更新-登入、註冊
    已可註冊並用該組帳密登入

--- js/sign_in.js
+++ js/sign_in.js
@@ -3,7 +3,7 @@ $(function () {
     var SHOW_CLASS = 'show',
         HIDE_CLASS = 'hide',
         ACTIVE_CLASS = 'active';
-
+    // 登入/註冊切換
     $('.tabs').on('click', 'li a', function (e) {
         e.preventDefault();
         var $tab = $(this),
@@ -22,12 +22,86 @@ $(function () {
             .addClass(SHOW_CLASS)
             .hide()
             .fadeIn(550);
-
-        // $('.active').css({
-        //     color: "#613134"
-        // });
-        // $('.hide').css({
-        //     color: "#C39681"
-        // });
     });
+
+    // 登入/註冊資料區
+    var acc =  new Map();
+    var password = [];
+    var email = new Map();
+
+    acc.set("test",0);
+    password[0]="000000";
+    email.set("test@test",0);
+
+    //註冊區
+    //帳號、密碼、再次輸入密碼、email
+    $(".btn_up").click(function () {
+        
+        const formElement = document.getElementById("form_up");
+        const accText = formElement[0].value;
+        const pwText = formElement[1].value;
+        const pwAgText = formElement[2].value;
+        const emailText = formElement[3].value;
+        var ero=false;
+
+        if(acc.has(accText)){
+            // 檢查帳號有沒有重複
+            alert("此帳號已有人使用");
+            ero=true;
+        }
+        
+        if(pwText.length < 6 ){
+            alert("密碼長度需六位以上");
+            ero=true;
+        }
+        if(pwText.length >= 6 && pwAgText != pwText){
+            // 判斷不同後清空密碼欄位
+            alert("兩次輸入密碼不同，請重新輸入");
+            formElement[1].value="";
+            formElement[2].value="";
+            ero=true;
+        }
+        if(email.has(emailText)){
+            // 檢查信箱有沒有重複
+            alert("此信箱已有人使用");
+            ero=true;
+        }
+        if(!ero){
+            //都沒有錯，才把資料存到陣列，並清空畫面
+            acc.set(accText,(acc.size));
+            password.push(pwText);
+            email.set(emailText,(email.size));
+            alert('註冊成功');
+            for(var i = 0 ;i<=3;i++){
+                formElement[i].value="";
+            }
+        }
+
+        return false; //參考 https://www.twblogs.net/a/5b7e9f592b717767c6aae1e9 送出時讓表單不要跳轉
+    });
+
+    
+    //登入區
+
+    $(".btn_in").click(function () {
+        const formElement = document.getElementById("form_in");
+        const accText = formElement[0].value;
+        const pwText = formElement[1].value;
+        var num="";
+
+        //如果num有取得值，表示陣列有這個帳號，如果num是null代表沒有
+        //密碼陣列同一個位置，如果密碼也一樣就可以登入
+        num=acc.get(accText);  //num型態是number
+        if(num !=null && pwText == password[num] ){
+            alert("登入成功");
+        }else if(num ==null){
+            alert("此帳號不存在");
+        }else if( pwText != password[num]){
+            alert("密碼錯誤");
+        };
+
+        return false;
+    });
+    
+
 });

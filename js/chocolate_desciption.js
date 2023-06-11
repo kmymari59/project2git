4b20aa5ce902e05f3989655571bcd9ffa1687ac0
kmymari59 <kmymari59@gmail.com>
2023-06-12 01:31:53 +0800

    更新-巧克力介紹

---
+++ js/chocolate_desciption.js
@@ -0,0 +1,7 @@
+//可參考 https://ithelp.ithome.com.tw/articles/10095237 
+// JQ套件可試 http://jquery-textfill.github.io/
+// 套件說明 https://github.com/jquery-textfill/jquery-textfill/blob/master/README.md
+
+$('.list_right').textfill({
+    
+});

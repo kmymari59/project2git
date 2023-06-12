879a37063dcc0cc6febf716f0644ac102b22b33c
kmymari59 <kmymari59@gmail.com>
2023-06-12 09:22:14 +0800

    更新-客製

--- js/chocolate_desciption.js
+++
@@ -1,7 +0,0 @@
-//可參考 https://ithelp.ithome.com.tw/articles/10095237 
-// JQ套件可試 http://jquery-textfill.github.io/
-// 套件說明 https://github.com/jquery-textfill/jquery-textfill/blob/master/README.md
-
-$('.list_right').textfill({
-    
-});

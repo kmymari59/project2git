818b74f70974a31d9121c145ccff9fe754ab5ed9
kmymari59 <kmymari59@gmail.com>
2023-06-16 13:32:55 +0800

    更新-巧克力介紹-篩選按鈕

--- js/chocolate_desciption.js
+++ js/chocolate_desciption.js
@@ -12,40 +12,66 @@ const allItems_li = document.getElementsByTagName('li')
 
 $(document).ready(function () {
 
-    if (selectedItem === 'morning') {
-        // 顯示"沁心晨曦"的內容
-        // 隱藏除了class為"morning"的項目以外的其他項目
+
+    if (selectedItem !== null) {
+        // 根據點來的項目，判斷要顯示哪一個
         for (let i = 0; i < allItems.length; i++) {
             const item = allItems[i];
-            if (!item.classList.contains('morning')) {
+            if (!item.classList.contains(selectedItem)) {
                 item.style.visibility = 'hidden';
                 item.style.height = 0;
+                item.style.marginTop="-10px";
+                item.style.marginBottom="-10px";
             }
         }
-        $(".check_list li").removeClass("active_color");
-        $(".check_list .morning").addClass("active_color");
-    }
 
-/*
-    // 監聽按鈕點擊事件
-    $(".check_list li").on("click", function (e) {
-        e.preventDefault(); // 防止點擊時頁面跳轉
-        
-        // 要顯示的巧克力區
-        var targetClass = $(this).attr("class"); // 取得被點擊的按鈕的 class
-        if (targetClass === "allItem") {
-            // 顯示全部商品
-            $("article").removeClass("hide"); // 移除 hide 類別
-        } else {
-            // 顯示目標類別的商品
-            $("article").addClass("hide"); // 加上 hide 類別
-            $("article." + targetClass).removeClass("hide"); // 移除 hide 類別
+        $(".check_list li").removeClass("active_color");//li先全部便沒選擇的樣式
+
+        if(selectedItem === "morning"){
+            $(".check_list .morning").addClass("active_color");
+        }else if(selectedItem === "afternoon"){
+            $(".check_list .afternoon").addClass("active_color");
+        }else if(selectedItem === "mature"){
+            $(".check_list .mature").addClass("active_color");
+        }else if(selectedItem === "relax"){
+            $(".check_list .relax").addClass("active_color");
         }
 
+    }
+// =========================================================================================================================================
 
-        // 被點選時要變色
-        $(".check_list li").removeClass("active_color");
-        $(this).addClass("active_color");
-    });
-*/
+        // 點擊按鈕時，判斷要顯示的項目
+        // 監聽按鈕點擊事件
+        $(".check_list li").on("click", function (e) {
+            e.preventDefault(); // 防止點擊時頁面跳轉
+            $(".check_list li").removeClass("active_color");//先全部移除active_color
+            var targetClass = $(this).attr("class"); // 取得被點擊的按鈕的 class
+
+            for (let i = 0; i < allItems.length; i++) {
+                const item = allItems[i];
+                if(targetClass!="allItem"){
+                    console.log("指定商品");
+                }else if(targetClass=="allItem"){
+                    console.log("全部商品");
+                }
+
+                if (!item.classList.contains(targetClass) && targetClass!="allItem") {
+                    item.style.visibility = 'hidden';
+                    item.style.height = 0;
+                    item.style.marginTop="-10px";
+                    item.style.marginBottom="-10px";
+                }else{
+                    item.style.visibility = 'visible';
+                    item.style.height = "unset";
+                    item.style.marginTop="unset";
+                    item.style.marginBottom="40px";
+                }
+            }
+            
+            
+            $(this).addClass("active_color");//再把作用中的選項加上active_color
+
+        });
+
+        
 });
